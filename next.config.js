7bea039822a0f2b52b3b2c81b3af3ed75f38ab69
Armand Abric <armand@forgebinaire.net>
2021-01-28 09:45:42 +0100

    Use Preact in production build

--- next.config.js
+++ next.config.js
@@ -7,4 +7,16 @@ module.exports = {
   env: {
     PUBLIC_PATH: publicPath.trim().replace(/\/$/, ""),
   },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 };
