39d7085b5d3b933769e211f264ce2a6d5dad94f3
Sébastien HOUZÉ <sebastien.houze@french-hospitality.fr>
2021-03-03 20:09:24 +0100

    refine baseline

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,4 @@
 export const BLOG_URL = process.env.PUBLIC_PATH;
 export const BLOG_TITLE = "Build Gogaille";
-export const BLOG_SUBTITLE = "We redefine the Hospitality world with the latest techs.";
-
-export const META_DESCRIPTION = "We redefine the Hospitality world with the latest techs.";
+export const BLOG_SUBTITLE = "We redefine the Hospitality world with the latest tech.";
+export const META_DESCRIPTION = "We redefine the Hospitality world with the latest tech.";
