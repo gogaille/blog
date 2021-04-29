062cb73ebc792a30def62315e609c5f6d4507707
SebF <sebastien.fezzany@french-hospitality.fr>
2021-04-29 17:39:55 +0200

    Remove bold class for Belly Display font

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { BLOG_TITLE } from "../globals";
 
 const Header = () => {
   return (
-    <p className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 font-belly-display">
+    <p className="text-2xl md:text-4xl tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 font-belly-display">
       <Link href="/">
         <a className="hover:underline">{BLOG_TITLE}</a>
       </Link>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,7 +3,7 @@ import { BLOG_TITLE, BLOG_SUBTITLE } from "../globals";
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 font-belly-display">
+      <h1 className="text-6xl md:text-8xl tracking-tighter leading-tight md:pr-8 font-belly-display">
         {BLOG_TITLE}.
       </h1>
       <h4 className="text-center text-secondary md:text-left text-lg mt-5 md:pl-8 font-public-sans">
