b32d0b5e53c2f2866baea3b6cca7c9b8045bf5e9
Sébastien HOUZÉ <sebastien.houze@french-hospitality.fr>
2021-04-30 11:30:06 +0200

    fix: remove unneeded bold font (#72)

--- src/components/more-stories.tsx
+++ src/components/more-stories.tsx
@@ -10,7 +10,7 @@ type Props = {
 const MoreStories = ({ postNodes }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight font-belly-display">
+      <h2 className="mb-8 text-6xl md:text-7xl tracking-tighter leading-tight font-belly-display">
         More Stories
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
