3fbc78acb21aa3c2f42c6663280d9878965e055c
Armand Abric <armand@forgebinaire.net>
2021-04-30 10:52:09 +0200

    fix: Fix canonical url on production #46

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const vercelUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : null;
+  : undefined;
 
 const publicPath = process.env.PUBLIC_PATH
   ? process.env.PUBLIC_PATH.trim().replace(/\/$/, "")
-  : null;
+  : undefined;
 
 if (!vercelUrl && !publicPath) {
   throw new Error(
@@ -14,7 +14,7 @@ if (!vercelUrl && !publicPath) {
 
 module.exports = {
   env: {
-    PUBLIC_PATH: vercelUrl || publicPath,
+    PUBLIC_PATH: publicPath ?? vercelUrl,
   },
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
